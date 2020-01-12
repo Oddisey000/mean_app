f2f3eb49345ab6a05dd157d8c57e43e9894d73ed
Oddisey000 <oddisey000@gmail.com>
2020-01-12 15:31:42 +0200

    Add basics to the application

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postSub: Subscription;
+
+  constructor(public postService: PostService) { }
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postSub = this.postService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+
+}
